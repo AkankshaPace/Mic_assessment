7301c288e539082a40775f5dd984fbb5f41c9d4b
Akanksha Gupta <gupta.akanksha11@gmail.com>
2016-12-01 12:37:45 -0500

    Adding code to print total items near Unpublished article

--- js/script.js
+++ js/script.js
@@ -14,9 +14,12 @@
 	});
 
 	mic.controller('mainController', function($scope,$http,$route,userPersistenceService,moment) {
+		//set initial limit to 10 items
 		$scope.limit= 10;
 		$scope.orderByField = userPersistenceService.getCookieData('orderByField');
 		$scope.reverseSort = !userPersistenceService.getCookieData('reverseSort');
+
+		//retrieving data from articles.json file
 	    $http.get('assets/articles.json')
 	        .success(function(data) {
 	            $scope.items=data;
@@ -25,28 +28,35 @@
 	        .error(function(data,status,error,config){
 	            $scope.items = [{heading:"Error",description:"Could not load json   data"}];
 	        });
-	    $scope.loadMore = function() {
-		  $scope.limit += 10;
-		  if($scope.limit == $scope.length){
-		  	 $http.get('assets/more-articles.json')
+
+	    //retrievinf more data from more-articles.json file    
+	    $http.get('assets/more-articles.json')
 	        .success(function(data1) {
-	            $scope.items = $scope.items.concat(data1);	
+	            $scope.items = $scope.items.concat(data1);
+	            $scope.length += data1.length;	
 	        })
 	        .error(function(data,status,error,config){
 	            $scope.items = [{heading:"Error",description:"Could not load json   data"}];
 	        });
-		  }
+
+	    // function to load 10 more items    
+	    $scope.loadMore = function() {
+		  $scope.limit += 10;
 		};
 
+		//setting cookie for sorting field
 		$scope.settingOrder = function(orderByField){
 			userPersistenceService.setCookieData("orderByField",orderByField);
 			return orderByField;
 		};
+
+		//setting cookie for order of sorting field
 		$scope.settingReverse = function(reverseSort){
 			userPersistenceService.setCookieData("reverseSort",reverseSort);
 			return reverseSort;
 		};
 
+		//parsing json date to date type
 		$scope.dateConverter = function(published_at){
 			d = new Date(published_at);
 			d = JSON.stringify(d);
@@ -54,6 +64,7 @@
 		};
 	});
 
+	//factory to set and get cookies
 	mic.factory("userPersistenceService", ["$cookies", function($cookies) {
 		return {
 			setCookieData: function(key, value) {
